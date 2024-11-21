4fbdfcf2f29224a69e5a9b92b4e2da3cdd6ffd59
Sammy <sammykumardemo@gmail.com>
2024-11-21 22:08:30 +0530

    remove unwanted code

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
-	// Bug - 130
-	int i=10;
-	int j=20;
+	
         SpringApplication.run(DemoApplication.class, args);
     }
 
